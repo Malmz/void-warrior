69e86a16d7c15ba524475085944bf7ff14e99672
Calle Malmz <callem217@gmail.com>
2017-05-22 09:18:19 +0200

    Add class for rotating rectangles

---
+++ VoidWarrior/RotSprite.cs
@@ -0,0 +1,45 @@
+﻿
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace VoidWarrior
+{
+    class RotSprite
+    {
+        private Texture2D texture;
+        private Vector2 TL, TR, BL, BR;
+        float rotation;
+        private Color color;
+        public RotSprite(Texture2D texture, Vector2 position, Vector2 size, Color color, float rotation)
+        {
+            this.texture = texture;
+            this.color = color;
+            this.rotation = rotation * (float)Math.PI / 180;
+
+            TL = new Vector2(-size.X / 2, -size.Y / 2);
+            TR = new Vector2( size.X / 2, -size.Y / 2);
+            BL = new Vector2(-size.X / 2,  size.Y / 2);
+            BR = new Vector2( size.X / 2,  size.Y / 2);
+
+            TL = new Vector2((float)((-size.X / 2) * Math.Cos(this.rotation) - (-size.Y / 2) * Math.Sin(this.rotation)));
+
+            TL.X = (float)(TL.X * Math.Cos(this.rotation) - TL.Y * Math.Sin(this.rotation));
+            TL.Y = (float)(TL.Y * Math.Sin(this.rotation) - TL.X * Math.Cos(this.rotation));
+
+            TR.X = (float)(TR.X * Math.Cos(this.rotation) - TR.Y * Math.Sin(this.rotation));
+            TR.Y = (float)(TR.Y * Math.Sin(this.rotation) - TR.X * Math.Cos(this.rotation));
+
+            BL.X = (float)(BL.X * Math.Cos(this.rotation) - BL.Y * Math.Sin(this.rotation));
+            BL.Y = (float)(BL.Y * Math.Sin(this.rotation) - BL.X * Math.Cos(this.rotation));
+
+            TL.X = (float)(TL.X * Math.Cos(this.rotation) - TL.Y * Math.Sin(this.rotation));
+            TL.Y = (float)(TL.Y * Math.Sin(this.rotation) - TL.X * Math.Cos(this.rotation));
+
+            TL = position;
+            TR = new Vector2(position.X + size.X, position.Y);
+            BL = new Vector2(position.X, position.Y + size.Y);
+            BR = size;
+        }
+    }
+}
